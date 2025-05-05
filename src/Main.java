ff3776dd539f15a8a563a49c2d0d51301e4ff2aa
khong trung son <khongtrungson2004@gmail.com>
2025-05-05 23:08:44 +0700

    xóa hàm test

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,4 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
-    public static void test(){
-        System.out.println("Hello, World!");
-    }
 }
